74cce5663655cbbd7c9734ab4be7264bbacb1ec3
Angela <angela.fiasca@gmail.com>
2014-10-21 15:54:04 -0700

    routing

--- Angela-Ben-Craig-Malvin/install/routes/index.js
+++ Angela-Ben-Craig-Malvin/install/routes/index.js
@@ -6,4 +6,61 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+router.get('/blockquote', function(req, res) {
+  res.render('blockquote', { title: 'Express' });
+});
+
+router.get('/buttons', function(req, res) {
+  res.render('buttons', { title: 'Express' });
+});
+
+router.get('/checkradio', function(req, res) {
+  res.render('checkradio', { title: 'Express' });
+});
+
+router.get('/colors', function(req, res) {
+  res.render('colors', { title: 'Express' });
+});
+
+router.get('/error', function(req, res) {
+  res.render('error', { title: 'Express' });
+});
+
+router.get('/fonts', function(req, res) {
+  res.render('fonts', { title: 'Express' });
+});
+
+router.get('/headings', function(req, res) {
+  res.render('headings', { title: 'Express' });
+});
+
+router.get('/icons', function(req, res) {
+  res.render('icons', { title: 'Express' });
+});
+
+router.get('/images', function(req, res) {
+  res.render('images', { title: 'Express' });
+});
+
+router.get('/index', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/inline', function(req, res) {
+  res.render('inline', { title: 'Express' });
+});
+router.get('/inputfields', function(req, res) {
+  res.render('inputfields', { title: 'Express' });
+});
+router.get('/layout', function(req, res) {
+  res.render('layout', { title: 'Express' });
+});
+router.get('/ordered', function(req, res) {
+  res.render('ordered', { title: 'Express' });
+});
+router.get('/paragraph', function(req, res) {
+  res.render('paragraph', { title: 'Express' });
+});
+router.get('/unordered', function(req, res) {
+  res.render('unordered', { title: 'Express' });
+});
+

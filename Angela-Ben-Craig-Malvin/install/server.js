c03ea01d174a48e0229b09a5edf721cfd645ab6c
Angela <angela.fiasca@gmail.com>
2014-10-21 10:50:02 -0700

    add jade back

--- Angela-Ben-Craig-Malvin/install/server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-
-app.listen(process.env.PORT || 3000);
-console.log('gitbook express server started');
